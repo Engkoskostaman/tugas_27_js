faf13e9f9121343d6e4f81bb416a021e2225aa93
Engkos <engkoskostaman97@gmail.com>
2019-08-14 14:03:53 +0700

    tugas 27 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas27.js/index.js
@@ -0,0 +1,9 @@
+function panggilNumber(){
+  var a ="No "+"Rumah "+"saya "+"30A";
+  hasil = a;
+  console.log(hasil);
+
+  hasil2= a;
+  console.log(hasil2);
+}
+panggilNumber()
---
+++ tugas27.js/index1.js
@@ -0,0 +1,10 @@
+function panggilNumber(){
+  var a ="Jalan Pangeran Tirtayasa NO 70 ";
+
+  hasil = a;
+  console.log(hasil);
+  hasil2= a;
+  console.log(hasil2);
+
+}
+panggilNumber()
